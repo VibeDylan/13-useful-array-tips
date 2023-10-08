02c8c2207c2b3bf9254ffc6b09c9f9752e147f0d
elloconception <dylanbourbotte6@gmail.com>
2023-10-08 21:14:29 +0200

    add tips for empty an array

---
+++ tips/empty-an-array.js
@@ -0,0 +1,11 @@
+let friends = [
+    { name: "John", email: "john@gmail.com" },
+    { name: "Antoine", email: "antoine@gmail.com" },
+    { name: "Lucas", email: "lucas@gmail.com" },
+    { name: "Véronique", email: "veronique@gmail.com" },
+    { name: "Lisa", email: "lisa@gmail.com" },
+    { name: "Estere", email: "estere@gmail.com" },
+];
+
+friends.length = 0;
+console.log(friends);
